3820cfa92a89f8db8d5950aee986f70a2b93a761
mdshahnawaz123 <41420658+mdshahnawaz123@users.noreply.github.com>
2026-03-24 15:02:25 +0400

    Initial commit

---
+++ Command/ParamCommand.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using RevitUI.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_Lab.Command
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ParamCommand : IExternalCommand
+    {
+        private static ParamFilter frm;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiapp = commandData.Application;
+            var app = uiapp.Application;
+            var uidoc = uiapp.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            try
+            {
+                if(doc != null)
+                {
+                    if(frm == null || !frm.IsVisible)
+                    {
+                        frm = new ParamFilter(doc, uidoc);
+                        frm.Show();
+                    }
+                    else
+                    {
+                        frm.Activate();
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                TaskDialog.Show("M",ex.Message);
+            }
+            return Result.Succeeded;
+        }
+    }
+}
