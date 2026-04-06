49b88744ff7fc1515773a5809748e11a3230b8b3
mdshahnawaz123 <41420658+mdshahnawaz123@users.noreply.github.com>
2026-04-06 14:48:04 +0400

    Update

---
+++ RevitUI/ExternalCommand/Mark/Markhandler.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitUI.ExternalCommand.Mark
+{
+    public class Markhandler : IExternalEventHandler
+    {
+        public void Execute(UIApplication app)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            return "Mark Updated";
+        }
+    }
+}
