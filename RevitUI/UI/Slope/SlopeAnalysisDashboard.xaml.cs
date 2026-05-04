635a6f9d9c286865c7b8734eb8434b189d66fd26
mdshahnawaz123 <41420658+mdshahnawaz123@users.noreply.github.com>
2026-05-04 17:13:06 +0400

    Fix: Removed Xceed dependency from Slope Analysis to fix Revit 2027 crash and modernized UI

--- RevitUI/UI/Slope/SlopeAnalysisDashboard.xaml.cs
+++ RevitUI/UI/Slope/SlopeAnalysisDashboard.xaml.cs
@@ -30,10 +30,27 @@ namespace RevitUI.UI.Slope
             if (settings.SlopeColors?.Count >= 3)
             {
                 try {
-                    PickC1.SelectedColor = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(settings.SlopeColors[0]);
-                    PickC2.SelectedColor = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(settings.SlopeColors[1]);
-                    PickC3.SelectedColor = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(settings.SlopeColors[2]);
+                    PickC1.Background = new System.Windows.Media.SolidColorBrush((System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(settings.SlopeColors[0]));
+                    PickC2.Background = new System.Windows.Media.SolidColorBrush((System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(settings.SlopeColors[1]));
+                    PickC3.Background = new System.Windows.Media.SolidColorBrush((System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(settings.SlopeColors[2]));
                 } catch { }
+            }
+        }
+
+        private void ColorPicker_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (sender is System.Windows.Controls.Border border && border.Background is System.Windows.Media.SolidColorBrush brush)
+            {
+                var current = brush.Color;
+                using (var dialog = new System.Windows.Forms.ColorDialog())
+                {
+                    dialog.Color = System.Drawing.Color.FromArgb(current.A, current.R, current.G, current.B);
+                    if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        var next = dialog.Color;
+                        border.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(next.A, next.R, next.G, next.B));
+                    }
+                }
             }
         }
 
@@ -46,36 +63,37 @@ namespace RevitUI.UI.Slope
 
             _handler.Unit = ComboUnit.SelectedIndex == 1 ? SlopeUnit.Percentage : SlopeUnit.Degrees;
 
+            var c1 = ((System.Windows.Media.SolidColorBrush)PickC1.Background).Color;
+            var c2 = ((System.Windows.Media.SolidColorBrush)PickC2.Background).Color;
+            var c3 = ((System.Windows.Media.SolidColorBrush)PickC3.Background).Color;
+
             _handler.Ranges.Clear();
-            _handler.Ranges.Add(CreateRange(TxtT1.Text, PickC1.SelectedColor));
-            _handler.Ranges.Add(CreateRange(TxtT2.Text, PickC2.SelectedColor));
-            _handler.Ranges.Add(CreateRange(TxtT3.Text, PickC3.SelectedColor));
+            _handler.Ranges.Add(CreateRange(TxtT1.Text, c1));
+            _handler.Ranges.Add(CreateRange(TxtT2.Text, c2));
+            _handler.Ranges.Add(CreateRange(TxtT3.Text, c3));
 
             // Save Settings
             var settings = DataLab.SettingsManager.Load();
             settings.SlopeThresholds = new System.Collections.Generic.List<double> { 
-                double.TryParse(TxtT1.Text, out double t1) ? t1 : 5, 
-                double.TryParse(TxtT2.Text, out double t2) ? t2 : 10, 
+                double.TryParse(TxtT1.Text, out double t1) ? t1 : 0, 
+                double.TryParse(TxtT2.Text, out double t2) ? t2 : 5, 
                 double.TryParse(TxtT3.Text, out double t3) ? t3 : 15 
             };
             settings.SlopeColors = new System.Collections.Generic.List<string> {
-                PickC1.SelectedColor?.ToString() ?? "#10B981",
-                PickC2.SelectedColor?.ToString() ?? "#F59E0B",
-                PickC3.SelectedColor?.ToString() ?? "#EF4444"
+                c1.ToString(),
+                c2.ToString(),
+                c3.ToString()
             };
             DataLab.SettingsManager.Save(settings);
 
             _externalEvent.Raise();
         }
 
-        private SlopeRange CreateRange(string thresholdStr, System.Windows.Media.Color? color)
+        private SlopeRange CreateRange(string thresholdStr, System.Windows.Media.Color color)
         {
             double threshold = 0;
             double.TryParse(thresholdStr, out threshold);
-
-            if (color == null) color = System.Windows.Media.Colors.Gray;
-            
-            return new SlopeRange(threshold, color.Value.R, color.Value.G, color.Value.B);
+            return new SlopeRange(threshold, color.R, color.G, color.B);
         }
 
         private void BtnClose_Click(object sender, RoutedEventArgs e)
@@ -83,10 +101,10 @@ namespace RevitUI.UI.Slope
             this.Close();
         }
 
-        protected override void OnMouseLeftButtonDown(System.Windows.Input.MouseButtonEventArgs e)
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            base.OnMouseLeftButtonDown(e);
-            this.DragMove();
+            if (e.ButtonState == System.Windows.Input.MouseButtonState.Pressed)
+                this.DragMove();
         }
 
         protected override void OnClosed(EventArgs e)
